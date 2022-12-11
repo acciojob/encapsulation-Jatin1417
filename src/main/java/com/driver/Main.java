02a1205e76ec385d819efad63d59426393e8ab16
Jatin1417 <chaudhari.jatin.18ee1164@gmail.com>
2022-12-11 18:09:18 +0530

    second commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String args[]){
+        RWOnly r = new RWOnly();
+
+       // r.name = "Jatin";
+       //System.out.print(r.name);
+       //The field RWOnly.name is not visible
+       // The field RWOnly.name is not visible
+       r.set("Jatin");
+       System.out.print(r.get());
+    }
 }
--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -3,6 +3,8 @@ package com.driver.test;
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
+import com.driver.RWOnly;
+
 public class TestCases {
     
 }
