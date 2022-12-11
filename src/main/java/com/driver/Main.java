7dc6983b7d08b41f82555ab6fb52d271912000f9
Jatin1417 <chaudhari.jatin.18ee1164@gmail.com>
2022-12-11 18:20:43 +0530

    new commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String args[]){
         RWOnly r = new RWOnly();
 
-       // r.name = "Jatin";
-       //System.out.print(r.name);
+        r.name = "Jatin";
+        System.out.print(r.name);
        //The field RWOnly.name is not visible
        // The field RWOnly.name is not visible
        r.set("Jatin");
